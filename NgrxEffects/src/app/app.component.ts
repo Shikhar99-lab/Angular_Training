eddf89e341ee99e2cd8a52170e7269326dcb5fa9
Shikhar <me.shikhar99@gmail.com>
2024-06-20 14:05:56 +0530

    20 june office

--- NgrxEffects/src/app/app.component.ts
+++ NgrxEffects/src/app/app.component.ts
@@ -5,11 +5,13 @@ import { State, Store, select } from '@ngrx/store';
 import { Actions } from '@ngrx/effects';
 import { peopleSelector } from './store/people/people.selector';
 import { AsyncPipe, JsonPipe } from '@angular/common';
+import { ReaderComponent } from '../components/reader/reader.component';
+import { WriterComponent } from '../components/writer/writer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe,JsonPipe],
+  imports: [AsyncPipe,JsonPipe, ReaderComponent, WriterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- NgrxEffects/src/components/reader/reader.component.ts
+++ NgrxEffects/src/components/reader/reader.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Actions } from '@ngrx/effects';
+import { select, Store } from '@ngrx/store';
+import { loadData } from '../../app/store/people/people.actions';
+import { peopleSelector } from '../../app/store/people/people.selector';
 
 @Component({
   selector: 'app-reader',
@@ -8,5 +12,12 @@ import { Component } from '@angular/core';
   styleUrl: './reader.component.css'
 })
 export class ReaderComponent {
+  data$ = this.store.pipe(select(peopleSelector));
+  data:any;
+  constructor(private store: Store, private actions$: Actions) {}
 
+  ngOnInit() {
+    this.store.dispatch(loadData());
+    this.data$.subscribe((data)=>this.data=data);
+  }
 }
